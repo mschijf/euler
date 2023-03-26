1871bd4c587d7c4e191098ee63e4177de3fbd999
mschijf <mschijf@gmail.com>
2023-03-26 13:58:24 +0200

    45 refactoring and improving

--- src/main/kotlin/euler/problem000/problem041_050/TriangularPentagonalHexagonal.kt
+++ src/main/kotlin/euler/problem000/problem041_050/TriangularPentagonalHexagonal.kt
@@ -7,13 +7,26 @@ import kotlin.math.min
 
 class TriangularPentagonalHexagonal: EulerExecutable {
 
+//    override fun solve(): Any {
+//        (285+1 .. 100_000L).forEach { i ->
+//            val triangle = i*(i+1)/2
+//            if (triangle.isHexagonal() && triangle.isPentagonal()) {
+//                return "$triangle"
+//            }
+//        }
+//        return "Not found"
+//    }
+
+    // every hexagonal is a triangular number.
+    // Therefore, start generating the hexagonals and only chck if it is a pentagon.
     override fun solve(): Any {
         (285+1 .. 100_000L).forEach { i ->
-            val triangle = i*(i+1)/2
-            if (triangle.isHexagonal() && triangle.isPentagonal()) {
-                return "$triangle"
+            val hex = i*(2*i-1)
+            if (hex.isPentagonal()) {
+                return "$i $hex"
             }
         }
         return "Not found"
     }
+
 }
