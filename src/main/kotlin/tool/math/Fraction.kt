e0b9bd72d0c6b2549ff71ecd3a27993ea7a878f9
mschijf <mschijf@gmail.com>
2023-04-02 10:17:27 +0200

    Improved and refactored Fraction class - added +, -, *, / also for Int - small performace improvement by not creating intermediate Fraction objects while doing basic operations

--- src/main/kotlin/tool/math/Fraction.kt
+++ src/main/kotlin/tool/math/Fraction.kt
@@ -3,46 +3,58 @@ package com.tool.math
 import kotlin.math.absoluteValue
 import kotlin.math.sign
 
-class Fraction(numerator: Int, denominator: Int) : Comparable<Fraction> {
+class Fraction(inNumerator: Int, inDenominator: Int) : Comparable<Fraction> {
 
-    val normalizedNumerator: Int
-    val normalizedDenominator: Int
+    val numerator: Int
+    val denominator: Int
     init {
-        val gcd = gcd(numerator.absoluteValue, denominator.absoluteValue)
-        if (denominator == 0) {
-            normalizedNumerator = if (numerator == 0) 0 else numerator.sign
-            normalizedDenominator = 0
-        } else if (numerator == 0) {
-            normalizedNumerator = 0
-            normalizedDenominator = denominator.sign
+        val gcd = gcd(inNumerator.absoluteValue, inDenominator.absoluteValue)
+        if (inDenominator == 0) {
+            numerator = if (inNumerator == 0) 0 else inNumerator.sign
+            denominator = 0
+        } else if (inNumerator == 0) {
+            numerator = 0
+            denominator = inDenominator.sign
         } else {
-            normalizedNumerator = numerator / gcd
-            normalizedDenominator = denominator / gcd
+            numerator = inNumerator / gcd
+            denominator = inDenominator / gcd
         }
     }
 
     operator fun times(other: Fraction) =
-        Fraction(this.normalizedNumerator * other.normalizedNumerator,
-            this.normalizedDenominator * other.normalizedDenominator)
+        Fraction(this.numerator * other.numerator,
+            this.denominator * other.denominator)
+    operator fun times(other: Int) =
+        Fraction(this.numerator * other, this.denominator)
 
-    operator fun div(other: Fraction) = this * Fraction(other.normalizedDenominator, other.normalizedNumerator)
+    operator fun div(other: Fraction) =
+        Fraction(this.numerator * other.denominator,
+            this.denominator * other.numerator)
+    operator fun div(other: Int) =
+        Fraction(this.numerator, this.denominator * other)
 
     operator fun plus(other: Fraction) =
-        Fraction(this.normalizedNumerator * other.normalizedDenominator + this.normalizedDenominator * other.normalizedNumerator,
-            this.normalizedDenominator * other.normalizedDenominator)
+        Fraction(this.numerator * other.denominator + this.denominator * other.numerator,
+            this.denominator * other.denominator)
+    operator fun plus(other: Int) =
+        Fraction(this.numerator + this.denominator * other, this.denominator)
 
-    operator fun minus(other: Fraction) = this + Fraction(-1*other.normalizedNumerator, other.normalizedDenominator)
+    operator fun minus(other: Fraction) =
+        Fraction(this.numerator * other.denominator - this.denominator * other.numerator,
+            this.denominator * other.denominator)
+    operator fun minus(other: Int) =
+        Fraction(this.numerator - this.denominator * other, this.denominator)
 
-    private fun doubleValue() = normalizedNumerator.toDouble() /normalizedDenominator.toDouble()
+    private fun doubleValue() = numerator.toDouble() /denominator.toDouble()
 
     override fun compareTo(other: Fraction): Int {
         if (this == other)
             return 0
 
-        if (normalizedDenominator == 0) {
-            return if (this.normalizedNumerator.sign == other.normalizedNumerator.sign)
+        if (denominator == 0) {
+            return if (this.numerator.sign == other.numerator.sign)
                 0
-            else if (this.normalizedNumerator.sign < other.normalizedNumerator.sign)
+            else if (this.numerator.sign < other.numerator.sign)
                 -1
             else
                 1
@@ -56,34 +68,34 @@ class Fraction(numerator: Int, denominator: Int) : Comparable<Fraction> {
 
     override fun equals(other: Any?): Boolean {
         return if (other is Fraction) {
-            normalizedNumerator == other.normalizedNumerator && normalizedDenominator == other.normalizedDenominator
+            numerator == other.numerator && denominator == other.denominator
         } else {
             super.equals(other)
         }
     }
-    override fun hashCode() = Pair(normalizedNumerator, normalizedDenominator).hashCode()
-    override fun toString() = "($normalizedNumerator / $normalizedDenominator)"
+    override fun hashCode() = Pair(numerator, denominator).hashCode()
+    override fun toString() = "($numerator / $denominator)"
 
     fun asDecimalString(): String {
         var decimalFraction = ""
         val remainderList = mutableListOf<Int>()
 
-        var remainder = (normalizedNumerator % normalizedDenominator) * 10
+        var remainder = (numerator % denominator) * 10
         remainderList.add(remainder)
-        while (remainder != 0 && remainder < normalizedDenominator) {
+        while (remainder != 0 && remainder < denominator) {
             remainder *= 10
             remainderList.add(remainder)
             decimalFraction += "0"
         }
 
         while (remainder != 0) {
-            decimalFraction += remainder / normalizedDenominator
+            decimalFraction += remainder / denominator
 
-            remainder = (remainder % normalizedDenominator) * 10
+            remainder = (remainder % denominator) * 10
             if (remainder == 0 || remainder in remainderList)
                 break
             remainderList.add(remainder)
-            while (remainder < normalizedDenominator) {
+            while (remainder < denominator) {
                 remainder *= 10
                 remainderList.add(remainder)
                 decimalFraction += "0"
@@ -93,31 +105,31 @@ class Fraction(numerator: Int, denominator: Int) : Comparable<Fraction> {
         val cycleLength = if (remainder != 0 && remainder in remainderList) remainderList.size - remainderList.indexOf(remainder) else 0
         val fixedFractionPart = decimalFraction.take(decimalFraction.length - cycleLength)
         val reciprocalCycle = if (cycleLength > 0) "(" + decimalFraction.takeLast(cycleLength) + ")" else ""
-        val beforeComma = normalizedNumerator / normalizedDenominator
+        val beforeComma = numerator / denominator
         val comma = if (decimalFraction.isNotEmpty()) "." else ""
         return "$beforeComma$comma$fixedFractionPart$reciprocalCycle"
     }
 
 
     fun reciprocalCycleLength(): Int {
-        var remainder = (normalizedNumerator % normalizedDenominator) * 10
+        var remainder = (numerator % denominator) * 10
         if (remainder == 0) {
             return 0
         }
 
         val remainderList = mutableListOf<Int>()
         remainderList.add(remainder)
-        while (remainder != 0 && remainder < normalizedDenominator) {
+        while (remainder != 0 && remainder < denominator) {
             remainder *= 10
             remainderList.add(remainder)
         }
 
         while (remainder != 0) {
-            remainder = (remainder % normalizedDenominator) * 10
+            remainder = (remainder % denominator) * 10
             if (remainder == 0 || remainder in remainderList)
                 break
             remainderList.add(remainder)
-            while (remainder < normalizedDenominator) {
+            while (remainder < denominator) {
                 remainder *= 10
                 remainderList.add(remainder)
             }
