bf58063534ecf3dc04cc31d104f3d78e31e8b333
mschijf <mschijf@gmail.com>
2023-04-07 17:14:22 +0200

    refactoring and improving int functions

--- src/main/kotlin/tool/math/IntFunctions.kt
+++ src/main/kotlin/tool/math/IntFunctions.kt
@@ -2,9 +2,16 @@ package tool.math
 
 /**
  * returns a list of the digits. The lowest digit will be put at index 0.
- * Example: 3456.toDigitList() = [3,4,5,6]
+ *
+ * Example: 3456.toDigitList() = [[3,4,5,6]]
  */
 fun Int.toDigitList(): List<Int> = this.toLong().toDigitList()
+
+/**
+ * returns a list of the digits. The lowest digit will be put at index 0.
+ *
+ * Example: 3456.toDigitList() = [[3,4,5,6]]
+ */
 fun Long.toDigitList(): List<Int> {
     val result = mutableListOf<Int>()
     var base = this
@@ -15,11 +22,12 @@ fun Long.toDigitList(): List<Int> {
     return result
 }
 
-fun Int.toDigitSet(): Set<Int> {
+fun Int.toDigitSet() = this.toLong().toDigitSet()
+fun Long.toDigitSet(): Set<Int> {
     val result = mutableSetOf<Int>()
     var base = this
-    while (base != 0) {
-        result.add( base % 10)
+    while (base != 0L) {
+        result.add( (base % 10).toInt())
         base /= 10
     }
     return result
@@ -33,10 +41,11 @@ fun Int.pow(power: Int):Long {
     return base
 }
 
-fun Int.log10(): Int {
+fun Int.log10(): Int = this.toLong().log10().toInt()
+fun Long.log10(): Long {
     var base = this
-    var c = -1
-    while (base > 0) {
+    var c = -1L
+    while (base > 0L) {
         base /= 10
         c++
     }
@@ -44,12 +53,14 @@ fun Int.log10(): Int {
 }
 
 
-fun Int.digitLength() = this.log10()+1
+fun Int.digitLength() = this.toLong().digitLength().toInt()
+fun Long.digitLength() = this.log10()+1
 
-fun Int.digitSum(): Int {
-    var sum = 0
+fun Int.digitSum() = this.toLong().digitSum().toInt()
+fun Long.digitSum(): Long {
+    var sum = 0L
     var base = this
-    while (base != 0) {
+    while (base != 0L) {
         sum += base % 10
         base /= 10
     }
